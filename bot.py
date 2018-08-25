3b48576950308c7a38eed11edabfb0731dfa3d50
BestCoderz <utk.bkkg@gmail.com>
2018-08-25 18:04:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -151,7 +151,7 @@ class Modmail(commands.Bot):
         return em
     
     @commands.command(pass_context=True)
-    @checks.is_owner()
+    @commands.check(is_owner)
     async def servers(self, ctx):
         """Lists and allows to leave servers"""
         owner = ctx.message.author
