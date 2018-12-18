f18f0800932f2439653ae9ccbc37652dfba07e21
BestCoderz <utk.bkkg@gmail.com>
2018-12-18 13:59:02 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -152,6 +152,39 @@ class Modmail(commands.Bot):
         em.set_footer(text='Thanks for adding our bot')
         return em
     
+    @commands.command(pass_context=True)
+    async def afk(self,ctx,*,reason : str):
+        user = ctx.message.author
+        msg = ctx.message
+        afk = open('afk.json').read()
+        afk = json.loads(afk)
+        afk[user.id] = reason
+        afk = json.dumps(afk)
+        x = await self.bot.say('You are now afk: {}'.format(reason))
+        with open('afk.json', 'w') as f:
+            f.write(afk)
+        await asyncio.sleep(5)
+        await self.bot.delete_messages([x,msg])
+
+
+    async def on_message(self,message):
+        user = message.author
+        channel = message.channel
+        afk = open('afk.json').read()
+        afk = json.loads(afk)
+        if user.id in afk:
+            del afk[user.id]
+            x = await self.bot.send_message(channel, 'You are now back from being afk.')
+        else:
+            mentions = message.mentions
+            for member in mentions:
+                if member.id in afk:
+                    y = await self.bot.send_message(channel, '**{}** is afk: *{}*'.format(member.name, afk[member.id]))
+        afk = json.dumps(afk)
+        with open('afk.json','w') as f:
+            f.write(afk)
+
+    
     @commands.command()
     @commands.has_permissions(administrator=True)
     async def setupserver(self, ctx, *, modrole: discord.Role=None):
